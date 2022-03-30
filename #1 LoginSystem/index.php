03ac2267e37f89e78cce29f9369e04ce2bd5c06d
Djody Kort <507946@vistacollege.nl>
2022-03-30 14:31:24 +0200

    Beginning of the Login screen
    Version: 1.0.1 Alpha

---
+++ #1 LoginSystem/index.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <title>Login System</title>
+    <link rel='stylesheet' href='./css/style.css'>
+</head>
+<body>
+<!-- Container -->
+<div class='container'>
+    <h1>Login System</h1>
+    <form action='#' method='POST'>
+        Username: <input type='text' name='username' placeholder='Username' id='formUsername'><br/>
+        Password: <input type='password' name='password' placeholder='Password' id='formPassword'><br/><br/>
+        <input type='submit' name='submit' value='Login' id='formSubmitButton'>
+    </form>
+</div>
+<!-- End Container -->
+</body>
+</html>
+
+<script src='./js/script.js'></script>
